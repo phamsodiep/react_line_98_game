2e2fb708b6568167e8449c1dfcc56257da6a5cdb
phamsodiep <phamduchuy@hotmail.com>
2020-04-17 19:58:02 +0700

    [src] implement exercise '1.3.1. Setup environments' of tutorial at phamsodiep.blogspot.com

---
+++ line_98_game/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+let msgElem = <i>Hello, world!</i>;
+ReactDOM.render(
+  <h1>{msgElem}</h1>,
+  document.getElementById('line98EntryPoint')
+);
