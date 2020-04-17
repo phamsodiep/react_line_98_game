6925ee6cc978240b4895153e7ac9554343d954b5
phamsodiep <phamduchuy@hotmail.com>
2020-04-17 21:26:20 +0700

    [src] implement exercise '2.3.2. Implement version 0.1.0' of tutorial at phamsodiep.blogspot.com

--- line_98_game/src/index.js
+++ line_98_game/src/index.js
@@ -2,14 +2,82 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
-class Hello extends React.Component {
+class GameBoard extends React.Component {
+  componentDidMount() {
+    let bgImg = document.getElementById("gameboard_bg");
+    if (bgImg === null) return;
+    let i = 0;
+    let n = 9; // dimension
+    let thick = 1;
+    let cellSize = 50;
+    let width = n * cellSize + thick * (n + thick);
+
+    // Context retrieving and initializing
+    bgImg.width = width;
+    bgImg.height = width;
+    bgImg.style.position = "absolute";
+    bgImg.style.left = 0;
+    bgImg.style.top = 0;
+    let canvasContext = bgImg.getContext("2d");
+    canvasContext.translate(0, 0);
+    canvasContext.lineWidth = 1;
+
+    // Render board surface
+    canvasContext.fillStyle = "#eeeeee";
+    canvasContext.fillRect(
+      0,
+      0,
+      width,
+      width
+    );
+
+    // Render board cells border
+    // |  |  |...
+    canvasContext.fillStyle = "#000000";
+    for (i = 0; i <= n; i++) {
+      let x = i * (cellSize + thick);
+      canvasContext.fillRect(x, 0, thick, width);
+    }
+    // =
+    for (i = 0; i <= n; i++) {
+      let y = i * (cellSize + thick);
+      canvasContext.fillRect(0, y, width, thick);
+    }
+  }
+
   render() {
-    let msgElem = <i>Hello, world!</i>;
-    return <h1>{msgElem}</h1> ;
+    // dimension * cellSize + lineWidth * (dimension + 1)
+    let width = 9 * 50 + 1 * (9 + 1);
+    let gameBoardStyle = {
+      position: "relative",
+      width: "100%",
+      height: "100%",
+      borderWidth: "0px",
+      borderStyle: "none",
+      margin: "0px 0px 0px 0px",
+      padding: "0px 0px 0px 0px"
+    };
+    let containerStyle = {
+      position: "static",
+      width: width + "px",
+      height: width + "px",
+      borderWidth: "0px",
+      borderStyle: "none",
+      margin: "0px 0px 0px 0px",
+      padding: "0px 0px 0px 0px"
+    };
+    return (
+      <div style={containerStyle}>
+        <div style={gameBoardStyle}>
+          <canvas id="gameboard_bg">
+          </canvas>
+        </div>
+      </div>
+    );
   }
 }
 
 ReactDOM.render(
-  <Hello />,
+  <GameBoard />,
   document.getElementById('line98EntryPoint')
 );
