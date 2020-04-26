9a72622e7ffaf93197a382ab25bd6a6bfafd9c69
phamsodiep <phamduchuy@hotmail.com>
2020-04-26 22:25:55 +0700

    [src] implement exercise '5.6. Exercise' of tutorial at phamsodiep.blogspot.com

--- line_98_game/src/index.js
+++ line_98_game/src/index.js
@@ -2,9 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
+const COLOUR_LIST = [
+  null,
+  "#00ccff",
+  "#ffff00",
+  "#33cc33",
+  "#0000ff",
+  "#cc3300",
+  "#ff66ff",
+  "#ff9900"
+];
+
 class ResetButton extends React.Component {
   resetGame() {
-    alert('Game reseting...');
+    //alert('Game reseting...');
+    gameBoardRef.resetGame();
   }
 
   render() {
@@ -13,6 +25,62 @@ class ResetButton extends React.Component {
     return (
       <div className={this.props.className} onClick={this.resetGame}>
         {caption}
+      </div>
+    );
+  }
+}
+
+class Ball extends React.Component {
+  getColourId() {
+    return this.props.colourId >= 0 && this.props.colourId <= 7 ?
+      this.props.colourId : 0;
+  }
+
+  getCellStyle() {
+    let thick = 1;
+    let cellSize = 50;
+    let size = cellSize + thick;
+
+    return {
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      margin: "auto",
+      position: "absolute",
+      width: size,
+      height: size,
+      left: this.props.left,
+      top: this.props.top
+    };
+  }
+
+  deriveBallStyle(colourId) {
+    let thick = 1;
+    let cellSize = 50;
+    let size = cellSize + thick;
+    let ballSize = Math.floor(size * 60.0 / 100.0);
+
+    return {
+      display: "block",
+      width: ballSize + "px",
+      height: ballSize + "px",
+      background: COLOUR_LIST[colourId],
+      borderRadius: "50%",
+      padding: "0px",
+      borderWidth: "0px",
+      borderStyle: "none",
+    };
+  }
+
+  render() {
+    let colourId = this.getColourId();
+    let cellStyle = this.getCellStyle();
+    let ballStyle = this.deriveBallStyle(colourId);
+
+    return (
+      <div style={cellStyle}>
+        <div style={ballStyle}>
+        </div>
       </div>
     );
   }
@@ -80,18 +148,73 @@ class GameBoard extends React.Component {
       margin: "0px 0px 0px 0px",
       padding: "0px 0px 0px 0px"
     };
+    let balls = this.createBalls();
+
     return (
       <div style={containerStyle}>
         <div style={gameBoardStyle}>
           <canvas ref={this.onCanvasCreate}>
           </canvas>
+          {balls}
         </div>
       </div>
     );
   }
+
+
+  //////////////////////////////////////////////////////////////////////////////
+  // BallManager interface
+  //////////////////////////////////////////////////////////////////////////////
+  resetGame() {
+    let dimension = 9;
+    let balls = [];
+    let i = 0;
+    let j = 0;
+    for (i = 0; i < dimension; i++) {
+      balls[i] = [];
+      for (j = 0; j < dimension; j++) {
+        balls[i][j] = 1; // cyan colour
+      }
+    }
+    // Update balls property of this.state by this function
+    // results in this.render() method invoked by React
+    this.setState((state, props) => ({
+      balls: balls
+    }));
+  }
+
+  createBalls() {
+    if (this.state === null) { // this.state is null if resetGame() is not invoked
+      return [];
+    }
+    let dimension = 9;
+    let balls = [];
+    let i = 0;
+    let j = 0;
+    for (i = 0; i < dimension; i++) {
+      for (j = 0; j < dimension; j++) {
+        balls[balls.length] = this.createBall(i, j, this.state.balls[i][j]);
+      }
+    }
+    return balls;
+  }
+
+  createBall(r, c, colourId) {
+    let thick = 1;
+    let cellSize = 50;
+    let sz = cellSize + thick;
+    let left = c * sz;
+    let top = r * sz;
+
+    return (<Ball
+      left={left}
+      top={top}
+      colourId={colourId}
+    />);
+  }
 }
 
-ReactDOM.render(
+var gameBoardRef = ReactDOM.render(
   <GameBoard />,
   document.getElementById('line98EntryPoint')
 );
